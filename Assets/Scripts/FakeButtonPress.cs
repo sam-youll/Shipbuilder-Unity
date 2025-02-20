5d51af43360aca84e2962c925fbc4c919085cb86
sydastry <165812966+sydastry@users.noreply.github.com>
2025-02-19 21:02:00 -0500

    pause enemy song
    u can now pause the enemy song

--- Assets/Scripts/FakeButtonPress.cs
+++ Assets/Scripts/FakeButtonPress.cs
@@ -7,7 +7,17 @@ public class FakeButtonPress : MonoBehaviour
     public Color hoverColor;
     public Color pressedColor;
     private Color currentColor;
-    
+
+    public bool isPlaying = false;
+
+    public Sprite triangle;
+    public Sprite square;
+
+    public GameObject otherTriangle;
+
+    private bool timerRunning = false;
+    private float targetTime;
+
     private SpriteRenderer sr;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,6 +33,18 @@ public class FakeButtonPress : MonoBehaviour
         var col = sr.color;
         col = Color.Lerp(col, currentColor, .1f);
         sr.color = col;
+    }
+
+    private void Update()
+    {
+        if (timerRunning)
+        {
+          targetTime -= Time.deltaTime;
+        }
+        if (targetTime < 0)
+        {
+            timerEnded();
+        }
     }
 
     private void OnMouseEnter()
@@ -42,5 +64,36 @@ public class FakeButtonPress : MonoBehaviour
         sr.color = pressedColor;
         currentColor = hoverColor;
         AudioManager.Instance.PlayEnemySong();
+
+        if (!timerRunning)
+        {
+            if (isPlaying)
+            {
+                sr.sprite = triangle;
+                otherTriangle.GetComponent<SpriteRenderer>().sprite = triangle;
+                AudioManager.Instance.StopEnemySong();
+                Debug.Log("stopping");
+                isPlaying = false;
+            }
+            else
+            {
+                //a smol timer so it doesnt just spam play/pause while the mouse is held down
+                timerStart();
+                sr.sprite = square;
+                otherTriangle.GetComponent<SpriteRenderer>().sprite = square;
+                Debug.Log("starting");
+                isPlaying = true;
+            }
+        }
+    }
+
+    void timerStart()
+    {
+        targetTime = 1;
+        timerRunning = true;
+    }
+    void timerEnded()
+    {
+        timerRunning = false;
     }
 }
