7ba1bf6fb9c304a4a3a8b4d21716ea9643cbf560
sam-youll <sf4112@nyu.edu>
2025-05-05 22:15:18 -0400

    geomag pulses ready to go

--- Assets/Scripts/CombatManager.cs
+++ Assets/Scripts/CombatManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -6,6 +7,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class CombatManager : MonoBehaviour
 {
@@ -46,12 +48,19 @@ public class CombatManager : MonoBehaviour
     public TextMeshPro battleNumberLabel;
     private int battleNumber;
 
+    public GameObject geomagneticPulse;
+    private bool pulsarEventActive;
+    private float pulseTimer = 4;
+
     public UnityEvent playerShipImpact;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         SceneManager.sceneLoaded += OnMainSceneLoaded;
+        
+        // geomagneticPulse.SetActive(false);
+        geomagneticPulse.transform.parent.gameObject.SetActive(false);
     }
 
     void OnMainSceneLoaded(Scene scene, LoadSceneMode mode)
@@ -73,6 +82,41 @@ public class CombatManager : MonoBehaviour
         
         if (state == State.inCombat)
         {
+            if (fightLevel == 2)
+            {
+                // geomagneticPulse.SetActive(true);
+                geomagneticPulse.transform.parent.gameObject.SetActive(true);
+                
+                pulseTimer -= Time.deltaTime;
+                if (pulseTimer <= 0)
+                {
+                    StartCoroutine(Pulsar());
+                    pulseTimer += 6;
+                    foreach (var weapon in playerShip.weapons)
+                    {
+                        var roll = Random.value;
+                        if (roll < 0.8f)
+                        {
+                            weapon.GetComponent<Weapon>().stunTimer += 3;
+                        }
+                    }
+
+                    foreach (var weapon in enemyShip.weapons)
+                    {
+                        var roll = Random.value;
+                        if (roll < 0.8f)
+                        {
+                            weapon.GetComponent<Weapon>().stunTimer += 3;
+                        }
+                    }
+                }
+                
+            }
+            else
+            {
+                // geomagneticPulse.SetActive(false);
+                geomagneticPulse.transform.parent.gameObject.SetActive(false);
+            }
             // if (Camera.main.transform.position != new Vector3(18, 0, -10))
             // {
             //     Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, new Vector3(18, 0, -10), .3f);
@@ -102,7 +146,9 @@ public class CombatManager : MonoBehaviour
                     }
 
                     enemyShip.maxHealth += 5;
+                    enemyShip.shield.GetComponent<Shield>().maxHealth += 2;
                     playerShip.maxHealth += 5;
+                    playerShip.shield.GetComponent<Shield>().maxHealth += 2;
                 }
                 
             }
@@ -158,5 +204,30 @@ public class CombatManager : MonoBehaviour
 
         AudioManager.Instance.ResetPlayedList();
         AudioManager.Instance.StopEnemySong();
+    }
+
+    IEnumerator Pulsar()
+    {
+        geomagneticPulse.transform.localScale = Vector3.one;
+        geomagneticPulse.SetActive(true);
+        var sr = geomagneticPulse.GetComponent<SpriteRenderer>();
+        var colcol = sr.color;
+        colcol.a = .6f;
+        sr.color = colcol;
+        while (geomagneticPulse.transform.localScale.x < 30)
+        {
+            geomagneticPulse.transform.localScale *= 1 + (Time.deltaTime * 7);
+            yield return null;
+        }
+        while (sr.color.a > 0)
+        {
+            var col = sr.color;
+            col.a -= Time.deltaTime;
+            sr.color = col;
+            yield return null;
+        }
+        colcol.a = .6f;
+        sr.color = colcol;
+        geomagneticPulse.SetActive(false);
     }
 }
