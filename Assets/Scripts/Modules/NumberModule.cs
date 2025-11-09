94e444b80b95c92ea3323402c977e939ce9445d8
sydastry <165812966+sydastry@users.noreply.github.com>
2025-11-09 16:16:17 -0600

    Added pedal tone module
    reverted back to simplest way, think advanced logic more for guide tones

---
+++ Assets/Scripts/Modules/NumberModule.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class NumberModule : SecondaryModule
+{
+    // we can set this to different sizes positive or negative
+    public float number = 1;
+
+    public override void Trigger(float value)
+    {
+        // Debug.Log($"AddModule on {gameObject.name} triggered base.Trigger() with a value of {value}.");
+        value = number;
+        base.Trigger(value, myInputIndex);
+    }
+
+    public override void Trigger()
+    {
+        // Debug.Log($"AddModule on {gameObject.name} triggered base.Trigger() with a value of {stepSize}.");
+        base.Trigger(number, myInputIndex);
+    }
+}
