c02c9fe6f59025a2edd5690faf7d58471389e0da
sydastry <165812966+sydastry@users.noreply.github.com>
2026-04-30 11:46:45 -0500

    harmonicizer script

--- Assets/Scripts/Modules/HarmonicModule.cs
+++ Assets/Scripts/Modules/HarmonicModule.cs
@@ -1,7 +1,24 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class HarmonicModule : PrimaryModule
 {
+    public enum Profile
+    {
+        neutral = 0,
+        combOdds = 1,
+        combEvens = 2,
+        naturalCurve = 3
+    }
+    public Profile profile = Profile.neutral;
+
+    private static List<Profile> profiles = new List<Profile>
+    {
+        Profile.neutral,
+        Profile.combOdds,
+        Profile.combEvens,
+        Profile.naturalCurve
+    };
     public override void Trigger(float value, int inputIndex)
     {
         switch (inputIndex)
@@ -10,6 +27,7 @@ public class HarmonicModule : PrimaryModule
                 base.Trigger();
                 break;
             case 1:
+                value %= profiles.Count;
                 MusicParams["harmonics"] = value;
                 // base.Trigger();
                 break;
